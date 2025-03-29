24002632de75f744851f3f172519f41547052a23
Taeyoung Song <restful3@gmail.com>
2025-03-30 08:45:09 +0900

    song

--- pyupbit/example/05_candle_month.py
+++
@@ -1,4 +0,0 @@
-import pyupbit
-
-df = pyupbit.get_ohlcv("KRW-BTC", "month")
-print(df)
--- pyupbit/example/06_ticks.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = "https://api.upbit.com/v1/trades/ticks?market=KRW-BTC&count=300"
-resp = requests.get(url)
-ticks = resp.json()
-
-for tick in ticks:
-    print(tick)
